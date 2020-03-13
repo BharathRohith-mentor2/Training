ff3884228c8db4973f8be0392c3c3638a8e80f25
mentor2.suntek <mentor2.suntek@gmail.com>
2020-03-13 16:08:29 +0530

    added P4- best code

---
+++ Week-1/P4 - ConvertToOne/Best.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Best
+{
+	public int getMinReplacements(int n)
+    {
+		if(n == 1)
+		{
+		    return 0;
+		}
+		else
+		{
+		    if( n%2 == 0)
+		    {
+				return 1 + getMinReplacements( n/2 );
+		    }
+		    return 1 + Math.min(getMinReplacements(n+1),getMinReplacements(n-1));
+		}
+    }
+    
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Best run  = new Best();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int minReplacements = run.getMinReplacements( n );
+		System.out.println(minReplacements);
+	}
+}
