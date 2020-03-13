48731a98d9cf02f2f8c17798b59322b0fd147449
mentor2.suntek <mentor2.suntek@gmail.com>
2020-03-13 16:06:26 +0530

    added P4-best code

--- Week-1/P2 - MinFlips/Worst.java
+++ Week-1/P2 - MinFlips/Worst.java
@@ -1,8 +1,29 @@
 import java.util.*;
 class Worst
 {
-	public static void main(String[] args)
+	public int getMinReplacements(int n)
+    {
+		if(n == 1)
+		{
+		    return 0;
+		}
+		else
+		{
+		    if( n%2 == 0)
+		    {
+				return 1 + getMinReplacements( n/2 );
+		    }
+		    return 1 + Math.min(getMinReplacements(n+1),getMinReplacements(n-1));
+		}
+    }
+    
+	public static void main (String[] args)
 	{
-		
+		// your code goes here
+		Worst run  = new Worst();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int minReplacements = run.getMinReplacements( n );
+		System.out.println(minReplacements);
 	}
 }
